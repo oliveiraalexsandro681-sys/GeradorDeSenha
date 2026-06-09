6691261ee171812834686395619c52a6bb51015e
Alexsandro-araujo <araujoalexsandro463@gmail.com>
2026-06-09 19:52:01 -0300

    Subindo GeradorDeSenhas

---
+++ GeradorDeSenhas/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace GeradorDeSenhas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.Gerar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gerador de senha :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 39);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(39, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 39);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 208);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(234, 39);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Senha Antiga";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(174, 114);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(187, 27);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(174, 166);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(187, 27);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(279, 220);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(187, 27);
+            this.txtSenha.TabIndex = 6;
+            // 
+            // Gerar
+            // 
+            this.Gerar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Gerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Gerar.ForeColor = System.Drawing.Color.White;
+            this.Gerar.Location = new System.Drawing.Point(46, 253);
+            this.Gerar.Name = "Gerar";
+            this.Gerar.Size = new System.Drawing.Size(379, 76);
+            this.Gerar.TabIndex = 7;
+            this.Gerar.Text = "Gerar";
+            this.Gerar.UseVisualStyleBackColor = true;
+            this.Gerar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1100, 562);
+            this.Controls.Add(this.Gerar);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Gerador de senha";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Button Gerar;
+    }
+}
+
---
+++ GeradorDeSenhas/Form1.cs
@@ -0,0 +1,53 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    namespace GeradorDeSenhas
+    {
+        public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void Form1_Load(object sender, EventArgs e)
+            {
+
+            }
+
+            private async void button1_Click(object sender, EventArgs e)
+            {
+                string nome = txtNome.Text;
+                string emeail = txtEmail.Text;
+                string senha = txtSenha.Text;
+
+                Random gerador = new Random();
+
+                int gere = gerador.Next(000000, 999999);
+                if (nome == "" && emeail ==  "")
+                {
+                    MessageBox.Show($"Digite o seu nome e email");
+                return;
+                }
+            else
+            {
+                MessageBox.Show($"Digite as informações acima");
+            }
+            MessageBox.Show($"Olá, {nome}, obrigado pelas informaçõe.");
+                MessageBox.Show("");
+                MessageBox.Show("Gerando senha....");
+                await Task.Delay(3000);
+                MessageBox.Show("");
+                MessageBox.Show($"Olá, {nome}, a sua senha é {gere} .");
+
+            }
+        }
+    }
